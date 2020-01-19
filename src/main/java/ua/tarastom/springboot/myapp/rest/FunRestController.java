fc0a215e1d61eb287b98c005f90bb2f640977006
HeavyMetal77 <ttm2006@ukr.net>
2020-01-19 21:33:42 +0200

    configuring spring-boot app using application.properties

--- src/main/java/ua/tarastom/springboot/myapp/rest/FunRestController.java
+++ src/main/java/ua/tarastom/springboot/myapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package ua.tarastom.springboot.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,18 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    //inject value from properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
+
 
     @GetMapping("/")
     public String sayHello() {
